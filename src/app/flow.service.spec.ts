8e5f660ff133b6fa12c9b59229073511a7e81184
William Denman <william.denman@gmail.com>
2021-10-30 05:50:39 +0100

    cleanup

--- src/app/flow.service.spec.ts
+++ src/app/flow.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { TestScheduler } from 'rxjs/testing';
 import { FlowService } from './flow.service';
+import { InitializationState, User } from './models';
 
 const testScheduler = new TestScheduler((actual, expected) => {
     expect(actual).toEqual(expected);
@@ -19,20 +20,18 @@ describe('Flow Service', () => {
  
     });
 
-
-
-    it('#initialization state is raised properly', () => {
+    it('#initialization state is retrieved when user logs in', () => {
         testScheduler.run((helpers) => {
             const { cold, expectObservable } = helpers;
-            const initializationState = cold('b');
-            const expected = 'b';
-        
-
+          
+            const initState: InitializationState = { FUSD: false, KittyItems: false, KittyItemsMarket: false, address: '0x12345'}
             //@ts-ignore
-            service.getInitializationState = () => initializationState;
+            service.getInitializationState = () => cold('-a', { a: initState});;
             
-            service.user.next({ addr: '0x12345', initializationState: { FUSD: false, KittyItems: false, KittyItemsMarket: false, address: '0x12345'}})
-            expectObservable(service.initializationState).toBe(expected);
+            const user: User = { addr: '0x12345' };
+            service.user.next(user);
+            
+            expectObservable(service.initializationState).toBe('-a', {a: initState});
         })
     });
 })
--- src/app/models.ts
+++ src/app/models.ts
@@ -1,6 +1,5 @@
 export interface User {
     addr: string
-    initializationState: InitializationState
   }
 
 export interface InitializationState {
