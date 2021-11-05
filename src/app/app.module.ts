e71a3d07294cd2694c0fa6d74941b3adf0d772af
William Denman <william.denman@gmail.com>
2021-11-05 07:29:11 +0100

    adds some material ui parts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FlowAuthComponent } from './flow-auth/flow-auth.component';
 import { FlowInitializationComponent } from './flow-initialization/flow-initialization.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
     FlowAuthComponent,
-    FlowInitializationComponent
+    FlowInitializationComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
